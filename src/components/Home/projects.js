257c5f4b5febb906fbe3006ad36d3e12ee29743a
Mintesnot Bekele <mintesnotbekele23@gmail.com>
2022-10-17 18:13:10 +0300

    projects added

--- src/components/Home/projects.js
+++ src/components/Home/projects.js
@@ -2,23 +2,44 @@ import { Row, Col, Divider } from "antd";
 import taxiyeImage from "../../assets/taxiye.png";
 import bodaImage from "../../assets/boda.png";
 import cattleImage from "../../assets/cattle.jpeg"
+import eltntImage from "../../assets/elnet.jpg"
 const Projects =()=>{
+    const contStyle = {
+        background: 'white',
+        borderRadius: '9%',
+        margin: '30px',
+        color: 'black'
+    }
 return (
    <> <Row><div style={{margin: 'auto' , fontSize: '28pt'}}>projects I have participated on</div></Row>
     <Row>
-        <Col span={8}><div style={{margin: '0px 100px'}}>
+        <Col style={contStyle} span={10}><div style={{margin: '0px 100px'}}>
             Taxiye
                   <img src={taxiyeImage}/>  
-            </div></Col>
-        <Col span={8}> <div style={{margin: '0px 100px'}}>
+         </div></Col>
+         <Col style={contStyle} span={10}><div style={{margin: '0px 100px'}}>
+         Boda Boda 
+            <img src={bodaImage}/>
+         </div></Col>
+         <Col style={contStyle} span={10}><div style={{margin: '0px 100px'}}>
+         ElTNT
+        <img src={eltntImage} style={{width: '100%'}}/>  
+         </div></Col>
+         <Col style={contStyle} span={10}><div style={{margin: '0px 100px'}}>
+         Yerras-App
+         <img src={cattleImage}/>
+         </div></Col>
+        {/* <Col  style={contStyle} span={10}> <div style={{margin: '0px 100px'}}> 
             Boda Boda 
             <img src={bodaImage}/>
             </div></Col>
-        <Col span={8}> <div style={{margin: '0px 100px'}}>ElTNT</div> </Col>
-        <Col span={8}> <div style={{margin: '0px 100px'}}>Yerras-App
+        <Col  style={contStyle} span={8}> <div style={{margin: '0px 100px'}}>ElTNT
+        <img src={taxiyeImage}/>  
+        </div> </Col>
+        <Col  style={contStyle} span={8}> <div style={{margin: '0px 100px'}}>Yerras-App
          <img src={cattleImage}/>
         </div></Col>
-        <Col span={8}> <div style={{margin: '0px 100px'}}>Berhan Lottery </div></Col>
+        <Col  style={contStyle} span={8}> <div style={{margin: '0px 100px'}}>Berhan Lottery </div></Col> */}
     </Row>
     </>
 )
